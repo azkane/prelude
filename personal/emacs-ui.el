0bfa9941e781e1539fda533b6d176194b76a36c1
azkane <azkane7@gmail.com>
2013-04-08 20:48:44 -0500

    More ui changes

--- personal/emacs-ui.el
+++ personal/emacs-ui.el
@@ -1,7 +1,24 @@
+(require 'uniquify)
+
+;; Disable GUI features
 (setq use-file-dialog nil)
 (setq use-dialog-box nil)
 (set-scroll-bar-mode nil)
 
+;; Cute cats 
 (nyan-mode 1)
 
+;; load tomorrow-bright
 (disable-theme 'zenburn)
+(load-theme 'sanityinc-tomorrow-bright) 
+
+;; line-numbers
+(global-linum-mode 1)
+
+(setq prelude-whitespace nil)
+
+;; Uniquify settings 
+(setq uniquify-buffer-name-style 'reverse)
+(setq uniquify-separator " • ")
+(setq uniquify-after-kill-buffer-p t)
+(setq uniquify-ignore-buffers-re "^\\*")
